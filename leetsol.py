498d01c22b6b9ac130d0c22d30573a6ca5187c2c
MuhammadShavaiz <shavaizsohail@gmail.com>
2024-05-03 10:22:45 -0700

    regular commit

--- leetsol.py
+++ leetsol.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from typing import List
+import re
 
 """
 Contains Duplicate:
@@ -60,7 +61,69 @@ class Solution:
             if complement in seen and seen[complement] != i:
                 return [i, seen[complement]]
 
+"""
+Valid Palindrome:
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters
+and removing all non-alphanumeric characters, it reads the same forward and backward. 
+Alphanumeric characters include letters and numbers.
+Given a string s, return true if it is a palindrome, or false otherwise.
+Example 1:
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+"""
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = re.sub(r'[^a-zA-Z0-9]', '', s)
+        s = s.lower()
+        rev_s = s[::-1]
+        if s == rev_s:
+            return True
+        else:
+            return False
 
+"""
+Best Time to Buy and Sell Stock:
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+Example 1:
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+"""
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        buy = prices[0]
+        for i in range(1, len(prices)):
+            if prices[i] < buy:
+                buy = prices[i]
+            elif prices[i] - buy > profit:
+                profit = prices[i] - buy
+        return profit
 
-
-
+"""
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+ determine if the input string is valid. An input string is valid if:
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+Example 1:
+Input: s = "()"
+Output: true
+"""
+class Solution:
+    def isvalid(self, s: str) -> bool:
+        stk = []
+        for i in s:
+            if i == "[" or i == "(" or i == "{":
+                stk.append(i)
+            else:
+                if len(stk) == 0 or \
+                        (i == ")" and stk.pop() != "(") or \
+                        (i == "}" and stk.pop() != "{") or \
+                        (i == "]" and stk.pop() != "["):
+                    return False
+        return not stk
