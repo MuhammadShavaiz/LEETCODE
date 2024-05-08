4920e9f6a9ed46b6660c7edd481617519a58d725
MuhammadShavaiz <shavaizsohail@gmail.com>
2024-05-08 10:44:43 -0700

    regular commit

--- leetsol.py
+++ leetsol.py
@@ -270,5 +270,14 @@ class Solution:
             anagram_map[sorted_word].append(word)
         return list(anagram_map.values())
 
-
-
+"""
+Top K Frequent Elements:
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+Example 1:
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+"""
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counter = Counter(nums)
+        return [num for num, _ in counter.most_common(k)]
