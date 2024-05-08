94487b97a542ef493515e210442ce0c20d4bf526
MuhammadShavaiz <shavaizsohail@gmail.com>
2024-05-08 10:24:19 -0700

    regular commit

--- leetsol.py
+++ leetsol.py
@@ -255,5 +255,20 @@ class Solution:
             if fast == slow:
                 return True
         return False
+"""
+Group Anagrams:
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+Example 1:
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+"""
+class Solution:
+    def groupAnagrams(self, strs):
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            anagram_map[sorted_word].append(word)
+        return list(anagram_map.values())
+
 
 
