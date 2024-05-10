dd05c32b61326af92c74c58cfe295e45cbc4cd52
MuhammadShavaiz <shavaizsohail@gmail.com>
2024-05-10 11:40:24 -0700

    regular commit

--- leetsol.py
+++ leetsol.py
@@ -281,3 +281,52 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         counter = Counter(nums)
         return [num for num, _ in counter.most_common(k)]
+"""
+3sum:
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+Notice that the solution set must not contain duplicate triplets.
+Example 1:
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+"""
+
+
+class Solution:
+    def threeSum(self, nums):
+        nums.sort()
+        answer = []
+
+        if len(nums) < 3:
+            return answer
+
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                break
+
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            low, high = i + 1, len(nums) - 1
+            while low < high:
+                s = nums[i] + nums[low] + nums[high]
+                if s > 0:
+                    high -= 1
+                elif s < 0:
+                    low += 1
+                else:
+                    answer.append([nums[i], nums[low], nums[high]])
+                    lastLowOccurrence, lastHighOccurrence = nums[low], nums[high]
+
+                    while low < high and nums[low] == lastLowOccurrence:
+                        low += 1
+
+                    while low < high and nums[high] == lastHighOccurrence:
+                        high -= 1
+
+        return answer
