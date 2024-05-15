071bacfb37688b8075f09008d9d326747f8f5973
Muhammad Shavaiz <shavaizsohail@gmail.com>
2024-05-15 23:03:39 +0500

    Update leetsol.py

--- leetsol.py
+++ leetsol.py
@@ -330,3 +330,30 @@ class Solution:
                         high -= 1
 
         return answer
+  """Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+"""
+    class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {")":"(", "}":"{", "]":"["}
+
+        for char in s:
+            if char in mapping.values():
+                stack.append(char)
+            elif char in mapping.keys():
+                if not stack or mapping[char] != stack.pop():
+                    return False
+        
+        return not stack
