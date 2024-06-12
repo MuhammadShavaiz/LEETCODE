b36f1a155f8e0a0aade7b3207297ee5eaa91debe
Muhammad Shavaiz <shavaizsohail@gmail.com>
2024-06-12 08:03:53 -0700

    initial commit

--- leetsol.py
+++ leetsol.py
@@ -439,6 +439,38 @@ class Solution:
                 return False
             my_dict[char] -= 1
         return True
+'''
+word pattern:
+Given a pattern and a string s, find if s follows the same pattern.
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
+Example 1:
+Input: pattern = "abba", s = "dog cat cat dog"
+Output: true
+Example 2:
+Input: pattern = "abba", s = "dog cat cat fish"
+Output: false
+Example 3:
+Input: pattern = "aaaa", s = "dog cat cat dog"
+Output: false
+'''
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split(" ")
+        d = {}
+        m = set()
+        if len(pattern)!=len(words):
+            return False
+        for i in range(len(pattern)):
+            if pattern[i] in d:
+                if d[pattern[i]] != words[i]:
+                    return False
+            else:
+                if words[i] not in m:
+                    d[pattern[i]] = words[i]
+                    m.add(words[i])
+                else:
+                    return False
+        return True
 
 
 
