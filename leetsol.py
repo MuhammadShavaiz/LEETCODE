729d53fbdd2410f7c45a03317916724aa12341e1
Muhammad Shavaiz <shavaizsohail@gmail.com>
2024-06-13 11:39:05 -0700

    initial commit

--- leetsol.py
+++ leetsol.py
@@ -514,5 +514,23 @@ class Solution:
             else:
                 ans += sym[s[i]]
         return ans
+'''
+Length of Last Word
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+A word is a maximal 
+substring
+consisting of non-space characters only.
+Example 1:
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+'''
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        words = s.split()
+        return len(words[-1])
+
+
+
 
 
