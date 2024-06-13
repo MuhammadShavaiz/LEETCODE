a52f06d6cf84545486ed05eb62b204e5d61a36c4
Muhammad Shavaiz <shavaizsohail@gmail.com>
2024-06-13 11:28:47 -0700

    initial commit

--- leetsol.py
+++ leetsol.py
@@ -472,6 +472,47 @@ class Solution:
                     return False
         return True
 
-
+'''
+Roman to Integer:
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+Example 1:
+Input: s = "III"
+Output: 3
+Explanation: III = 3.
+Example 2:
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+Example 3:
+Input: s = "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+'''
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        sym = {
+            "I" : 1,
+            "V" : 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D":500,
+            "M":1000,
+             }
+        ans = 0
+        for i in range(len(s)):
+            if i < len(s)-1 and sym[s[i]] < sym[s[i+1]]:
+                ans -= sym[s[i]]
+            else:
+                ans += sym[s[i]]
+        return ans
 
 
